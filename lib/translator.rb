6858f2f12ed55aa2f55814e816b901becda1787d
Pramod Thomas <pmod23@gmail.com>
2020-05-27 19:09:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,18 +2,7 @@ require 'yaml'
 
 
 def load_library(fp*)
-   yml_hash = YAML.load_file(fp.to_s)
-   
-   
-   yml_hash.each do |key, value|
-     
-     new_hash = {}
-     new_hash[:english] = value[0]
-     new_hash[:japanese] = value[1]
-     value = new_hash
-     
-   end
-   yml_hash
+
 end
 
 def get_japanese_emoticon
